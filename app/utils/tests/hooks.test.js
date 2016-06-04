6c2514e8ed0d3f619204e326787961d531e02a06
Justin <justwright@me.com>
2016-06-04 14:22:45 -0700

    Updated indentation

--- app/utils/tests/hooks.test.js
+++ app/utils/tests/hooks.test.js
@@ -1,6 +1,6 @@
 /**
- * Test hooks
- */
+* Test hooks
+*/
 
 import expect from 'expect';
 import configureStore from 'store.js';
@@ -9,9 +9,9 @@ import { put } from 'redux-saga/effects';
 import { fromJS } from 'immutable';
 
 import {
-  injectAsyncReducer,
-  injectAsyncSagas,
-  getHooks,
+    injectAsyncReducer,
+    injectAsyncSagas,
+    getHooks,
 } from 'utils/hooks';
 
 // Fixtures
@@ -19,70 +19,70 @@ import {
 const initialState = fromJS({ reduced: 'soon' });
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'TEST':
-      return state.set('reduced', action.payload);
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case 'TEST':
+        return state.set('reduced', action.payload);
+        default:
+        return state;
+    }
 };
 
 const sagas = [
-  function* testSaga() {
-    yield put({ type: 'TEST', payload: 'yup' });
-  },
+    function* testSaga() {
+        yield put({ type: 'TEST', payload: 'yup' });
+    },
 ];
 
 describe('hooks', () => {
-  let store;
+    let store;
 
-  describe('getHooks', () => {
-    before(() => {
-      store = configureStore({}, memoryHistory);
+    describe('getHooks', () => {
+        before(() => {
+            store = configureStore({}, memoryHistory);
+        });
+
+        it('given a store, should return all hooks', () => {
+            const { injectReducer, injectSagas } = getHooks(store);
+
+            injectReducer('test', reducer);
+            injectSagas(sagas);
+
+            const actual = store.getState().get('test');
+            const expected = initialState.merge({ reduced: 'yup' });
+
+            expect(actual.toJS()).toEqual(expected.toJS());
+        });
     });
 
-    it('given a store, should return all hooks', () => {
-      const { injectReducer, injectSagas } = getHooks(store);
+    describe('helpers', () => {
+        before(() => {
+            store = configureStore({}, memoryHistory);
+        });
 
-      injectReducer('test', reducer);
-      injectSagas(sagas);
+        describe('injectAsyncReducer', () => {
+            it('given a store, it should provide a function to inject a reducer', () => {
+                const injectReducer = injectAsyncReducer(store);
 
-      const actual = store.getState().get('test');
-      const expected = initialState.merge({ reduced: 'yup' });
+                injectReducer('test', reducer);
 
-      expect(actual.toJS()).toEqual(expected.toJS());
+                const actual = store.getState().get('test');
+                const expected = initialState;
+
+                expect(actual.toJS()).toEqual(expected.toJS());
+            });
+        });
+
+        describe('injectAsyncSagas', () => {
+            it('given a store, it should provide a function to inject a saga', () => {
+                const injectSagas = injectAsyncSagas(store);
+
+                injectSagas(sagas);
+
+                const actual = store.getState().get('test');
+                const expected = initialState.merge({ reduced: 'yup' });
+
+                expect(actual.toJS()).toEqual(expected.toJS());
+            });
+        });
     });
-  });
-
-  describe('helpers', () => {
-    before(() => {
-      store = configureStore({}, memoryHistory);
-    });
-
-    describe('injectAsyncReducer', () => {
-      it('given a store, it should provide a function to inject a reducer', () => {
-        const injectReducer = injectAsyncReducer(store);
-
-        injectReducer('test', reducer);
-
-        const actual = store.getState().get('test');
-        const expected = initialState;
-
-        expect(actual.toJS()).toEqual(expected.toJS());
-      });
-    });
-
-    describe('injectAsyncSagas', () => {
-      it('given a store, it should provide a function to inject a saga', () => {
-        const injectSagas = injectAsyncSagas(store);
-
-        injectSagas(sagas);
-
-        const actual = store.getState().get('test');
-        const expected = initialState.merge({ reduced: 'yup' });
-
-        expect(actual.toJS()).toEqual(expected.toJS());
-      });
-    });
-  });
 });
